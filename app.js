2de838005b3ea223839609438d6aadc87726a68b
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2024-08-04 17:00:33 -0500

    Exercises complete

--- app.js
+++ app.js
@@ -5,25 +5,25 @@ console.log("EXERCISE 1:\n==========\n");
 
 function printOdds(count) {
     if (count < 1) {
-        alert("Count is less than 1");
+        console.log("Count is less than 1");
     }
     else {
         for (let i=1; i<=count; i++) {
-            if (i % 2 == 1) {
-                console.log(i + " is odd");
-            }
+            if (i % 2 === 1) console.log(i + " is odd");
         }
     }
 }
 
 printOdds(10);
+printOdds(19);
+printOdds(0);
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
-function checkAge(age) {
-    let above16 = "You can drive!";
-    let below16 = "Sorry, but you need to wait until you're 16.";
+function checkAge(userName="John Travolta", age=65) {
+    let above16 = `${userName} can drive!!!`;
+    let below16 = `Sorry, ${userName}, but you need to wait until you're 16.`;
 
     if (age < 16) {
         console.log(below16);
@@ -33,38 +33,22 @@ function checkAge(age) {
     }
 }
 
-checkAge(20);
+checkAge("Kelly", 20);
+checkAge("Lisa", 15);
+checkAge();
 
-// Exercise 2 Bonus Section
-console.log("EXERCISE 2: BONUS \n==========\n");
-
-function checkAgeBonus(name, age) {
-    age = prompt("How old are you?");
-
-    let above16 = "You can drive!";
-    let below16 = "Sorry, but you need to wait until you're 16.";
-
-    if (age < 16) {
-        console.log(below16);
-    }
-    else {
-        console.log(above16);
-    }
-}
-
-checkAgeBonus();
 
 // Exercise 3 Section
 console.log("EXERCISE 3:\n==========\n");
 
 function cartesian(x,y) {
 
-    if (x==0 && y==0){
+    if (x===0 && y===0){
         console.log(`${x}, ${y} is on the origin`);
 
-    } else if (x == 0) {
+    } else if (x === 0) {
             console.log (x + "," + y + " is on the x axis");
-        } else if (y == 0) {
+        } else if (y === 0) {
                 console.log (x + "," + y + " is on the y axis");
             }
     
@@ -99,21 +83,48 @@ function cartesian(x,y) {
 // Exercise 4 Section
 console.log("EXERCISE 4:\n==========\n");
 
-var type;
-
-function triangle(a,b,c) {
-    
-    if(a + b <= c && a + c >= b && b + c >= a) {
+function triangle(a, b, c) {
+    let type = "";
+    if (a + b <= c || a + c <= b || b + c <= a) {
         type = "invalid";
-    } else if (a == b && b == c) {
+    } else if (a === b && b === c) {
         type = "equilateral";
-    } else if (a == b || a == c || b == c) {
+    } else if (a === b || a === c || b === c) {
         type = "isosceles";
     } else {
         type = "scalene";
     }
+    return type;
 }
 
-triangle(2,4,5);
-console.log(`Triangle type is ${type}`);
+console.log(`Triangle of 2, 4, 5: type is ${triangle(2, 4, 5)}`);
+console.log(`Triangle of 1, 2, 2: type is ${triangle(1, 2, 2)}`);
+console.log(`Triangle of 1, 1, 2: type is ${triangle(1, 1, 2)}`);
 
+// Exercise 5 Section
+console.log("EXERCISE 5:\n==========\n");
+
+function dataUsage(planLimit, day, usage) {
+    const avgDailyPlan = planLimit / 30;
+    const avgDailyActual = usage / day;
+    const diff = avgDailyPlan - avgDailyActual;
+
+    console.log(`${day} days used, ${30 - day} days remaining.`);
+    console.log(`Average daily use: ${avgDailyPlan.toFixed(2)} GB/day.`);
+
+    let phrase = "";
+    if (diff > 0) phrase = "BELOW";
+    else if (diff === 0) phrase = "EQUALLING";
+    else phrase = "EXCEEDING";
+
+    console.log(`You are ${phrase} your average daily use (${avgDailyActual.toFixed(2)} GB/day).`);
+    if (diff < 0) {
+        console.log("If you continue this high usage, you'll exceed your data plan");
+        console.log(`by ${((30 - day) * avgDailyActual - planLimit + usage).toFixed(2)} GB.`);
+    }
+    console.log(`To stay on your data plan, use no more than ${((planLimit - usage) / (30 - day)).toFixed(2)} GB/day.`);
+}
+
+dataUsage(100, 15, 56);
+dataUsage(100,15,50);
+dataUsage(100, 2, 56);
